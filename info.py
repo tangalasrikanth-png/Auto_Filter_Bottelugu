05f60c688a1fed10e9df31d3127046b2e1f8524b
tangalasrikanth-png <tangalasrikanth@gmail.com>
2025-10-30 10:23:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://srikanthreddycmd77:KzkIWsMnrv0tDxS3@cluster0.hquo2bt.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Anudeep:Samiha3519@cluster0.03nh9pe.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'telugumovies_files') # Collection name (default: dreamcinezone_files)
 
